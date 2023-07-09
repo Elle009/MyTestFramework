351694a0132e82d538697f08ba4c8562bd9b96b7
Elle009 <bulgakovaeleonora@gmail.com>
2023-07-09 21:56:43 +0300

    Initial commit

---
+++ src/main/java/IssuePage.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class IssuePage extends BasePage {
+    By newIssueButton = By.xpath("//span[contains(text(), 'New issue')]/parent::a");
+    By issueField = By.id("issue_title");
+    By submitIssueButton = By.xpath("//button[contains (@class, 'btn-primary btn ml-2')]");
+    By newIssue = By.xpath("//a[contains(text(), 'New issue2')]");
+
+    public IssuePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public WebElement getNewIssueButton() {
+        return driver.findElement(newIssueButton);
+    }
+
+    public IssuePage createNewIssue(){
+        driver.findElement(newIssueButton).click();
+        driver.findElement(issueField).sendKeys("New issue2");
+        driver.findElement(submitIssueButton).click();
+        driver.findElement(newIssue).getText();
+        return this;
+    }
+}
